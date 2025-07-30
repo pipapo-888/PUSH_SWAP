4f9465be6392935d6bfcc281ef8de3e919d739e3
Kazuya Nomura <knomura@c4r3s9.42tokyo.jp>
2025-07-30 15:34:15 +0900

    first

---
+++ ft_push_swap.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_swap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 12:42:16 by knomura           #+#    #+#             */
+/*   Updated: 2025/07/15 13:52:09 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	char	world[] = "world";
+	int		len = ft_strlen(world);
+
+	ft_printf("Hello %s  %d", world, len);
+
+	return (0);
+}
