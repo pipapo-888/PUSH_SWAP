98f6954bc7eb4bdc6aa5aedfac5bb707be48f564
pipapo-888 <kazuya2002uk@gmail.com>
2025-07-31 20:01:09 +0900

    ヘッダーファイル

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:54:13 by knomura           #+#    #+#             */
-/*   Updated: 2025/07/15 13:51:51 by knomura          ###   ########.fr       */
+/*   Updated: 2025/07/31 19:09:48 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,20 @@
 #include "LIBFT/libft.h"
 #include "PRINTF/ft_printf.h"
 
+#define MAX_SIZE 1000
+
+void swap_a(t_stack stack);
+void swap_b(t_stack stack);
+void ss(t_stack stack);
+
+typedef struct {
+	int data[MAX_SIZE];
+	int size;
+} t_stack;
+
+typedef struct {
+	t_stack a;
+	t_stack b;
+} t_stacks;
 
 #endif
