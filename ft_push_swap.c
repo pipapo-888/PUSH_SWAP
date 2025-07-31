0b41db9888bdef23a809ca5dc9b48fc686dc710e
pipapo-888 <kazuya2002uk@gmail.com>
2025-07-31 13:25:09 +0900

    配列受取

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:42:16 by knomura           #+#    #+#             */
-/*   Updated: 2025/07/15 13:52:09 by knomura          ###   ########.fr       */
+/*   Updated: 2025/07/31 13:24:35 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,33 @@
 
 int	main(int argc, char *argv[])
 {
-	char	world[] = "world";
-	int		len = ft_strlen(world);
+	int a[argc];
+	int i;
 
-	ft_printf("Hello %s  %d", world, len);
+	i = 0;
+	ft_printf("%d\n", argc);
+
+	while (i < argc - 1)
+	{
+		a[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
+
+
+	push_swap(a);
 
 	return (0);
+}
+
+void push_swap(int *a)
+{
+
+	int i = 0;
+
+	for (int i = 0; a[i]; i++)
+	{
+		ft_printf("%d", a[i]);
+	}
+
+
 }
