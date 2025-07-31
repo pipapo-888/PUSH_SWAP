8597e462d3add602aee5e0243ba724521daa481a
pipapo-888 <kazuya2002uk@gmail.com>
2025-07-31 20:00:57 +0900

    command

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,42 +6,45 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:42:16 by knomura           #+#    #+#             */
-/*   Updated: 2025/07/31 13:24:35 by knomura          ###   ########.fr       */
+/*   Updated: 2025/07/31 18:06:33 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
+void push_swap(t_stacks stack, int element)
+{
+	int i = 0;
+	int b[element + 1];
+
+	swap_a(stack);
+	for (int i = 0; i < element; i++)
+		ft_printf("%d\n", stack.a.data[i]);
+
+
+
+
+}
+
 int	main(int argc, char *argv[])
 {
-	int a[argc];
+	t_stacks stack;
 	int i;
+	int temp;
 
 	i = 0;
-	ft_printf("%d\n", argc);
+	printf("%d\n", argc);
 
 	while (i < argc - 1)
 	{
-		a[i] = ft_atoi(argv[i + 1]);
+		temp = ft_atoi(argv[i + 1]);
+		stack.a.data[i] = temp;
 		i++;
 	}
 
 
-	push_swap(a);
+	push_swap(stack, argc - 1);
 
 	return (0);
-}
-
-void push_swap(int *a)
-{
-
-	int i = 0;
-
-	for (int i = 0; a[i]; i++)
-	{
-		ft_printf("%d", a[i]);
-	}
-
-
 }
---
+++ swap.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:43:00 by knomura           #+#    #+#             */
+/*   Updated: 2025/07/31 18:05:17 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void swap_a(t_stacks stack)
+{
+	int temp;
+
+	temp = stack.a.data[0];
+	stack.a.data[0] = stack.a.data[1];
+	stack.a.data[1] = temp;
+	ft_printf("sa\n");
+}
+
+void swap_b(t_stacks stack)
+{
+	int temp;
+
+	temp = stack.b.data[0];
+	stack.b.data[0] = stack.b.data[1];
+	stack.b.data[1] = temp;
+	ft_printf("sb\n");
+}
+
+void ss(t_stacks stack)
+{
+	int temp;
+
+	temp = stack.a.data[0];
+	stack.a.data[0] = stack.a.data[1];
+	stack.a.data[1] = temp;
+
+	temp = stack.b.data[0];
+	stack.b.data[0] = stack.b.data[1];
+	stack.b.data[1] = temp;
+
+	ft_printf("ss\n");
+}
