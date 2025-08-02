d699fc5b5756dd957f4f105e22878ef36120fb82
pipapo-888 <kazuya2002uk@gmail.com>
2025-08-02 17:10:06 +0900

    プッシュ完成

--- push.c
+++ push.c
@@ -6,25 +6,44 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:41:53 by knomura           #+#    #+#             */
-/*   Updated: 2025/07/31 20:09:40 by knomura          ###   ########.fr       */
+/*   Updated: 2025/08/02 15:06:50 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void push_a(t_stacks stack)
-// {
-// 	if (!b[0])
-// 		return;
-// 	for ()
-
-
-
-// }
-
-void push_b(int *a, int *b)
+void push_a(t_stacks *stack)
 {
+	if (stack->b.size == 0)
+		return;
+	for (int i = stack -> a.size; i > 0; i--)
+	{
+		stack->a.data[i] = stack->a.data[i - 1];
+	}
+	stack->a.data[0] = stack->b.data[0];
+	stack->a.size++;
+	stack->b.size--;
+	for (int i = 0; i < stack->b.size; i++)
+	{
+		stack->b.data[i] = stack->b.data[i + 1];
+	}
+	printf("pa\n");
+}
 
-
-
+void push_b(t_stacks *stack)
+{
+	if (stack->a.size == 0)
+		return;
+	for (int i = stack -> b.size; i > 0; i--)
+	{
+		stack->b.data[i] = stack->b.data[i - 1];
+	}
+	stack->b.data[0] = stack->a.data[0];
+	stack->a.size--;
+	stack->b.size++;
+	for (int i = 0; i < stack->a.size; i++)
+	{
+		stack->a.data[i] = stack->a.data[i + 1];
+	}
+	printf("pb\n");
 }
