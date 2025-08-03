9cb562454797584e29f76ba9b16660a89ac96e2e
pipapo-888 <kazuya2002uk@gmail.com>
2025-08-03 15:05:18 +0900

    push_swap用atoi作成

---
+++ utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 12:44:03 by knomura           #+#    #+#             */
+/*   Updated: 2025/08/03 14:16:13 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <limits.h>
+
+int	ft_safe_atoi(const char *str, int *error)
+{
+	long	result;
+	int	minus;
+	int	i;
+
+	minus = 1;
+	result = 0;
+	i = 1;
+	if (!(str[0] == '-' || str[0] == '+' ||(str[0] >= '0' && str[0] <= '9')))
+			*error = 1;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			*error = 1;
+		i++;
+	}
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			minus *= -1;
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[i++] - '0');
+	if (minus * result > INT_MAX || minus * result < INT_MIN)
+		*error = 1;
+	return (minus * result);
+}
