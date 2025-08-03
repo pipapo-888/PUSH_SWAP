b629e086940031ebaf29f8c81a94e80b86226f1c
pipapo-888 <kazuya2002uk@gmail.com>
2025-08-03 15:04:54 +0900

    ソート追加　pushswap関数にコピーではなくアドレス送る

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,43 +6,84 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:42:16 by knomura           #+#    #+#             */
-/*   Updated: 2025/08/02 17:52:21 by knomura          ###   ########.fr       */
+/*   Updated: 2025/08/03 15:02:26 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
-void push_swap(t_stacks stack, int element)
+int	is_sorted(t_stack a)
+{
+	int	i;
+
+	i = 0;
+	while (i < a.size - 1)
+	{
+		if (a.data[i] > a.data[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void sort_2(t_stacks *stack)
+{
+	if (is_sorted(stack -> a))
+	{
+		printf("ソート済み\n");
+		return ;
+	}
+	else
+		swap_a(stack, 1);
+	printf("ソートしたよ\n");
+}
+
+void sort_3(t_stacks *stack)
+{
+	if (is_sorted(stack -> a))
+	{
+		printf("ソート済み\n");
+		return ;
+	}
+	
+}
+
+void push_swap(t_stacks *stack, int element)
 {
 	int i = 0;
 	int b[element + 1];
 
-	// swap_a(&stack, 1);
-	// swap_b(&stack, 1);
-	// ss(&stack);
-	// push_a(&stack);
-	// push_b(&stack);
-	// rotate_a(&stack, 1);
-	// rotate_b(&stack, 1);
-	// rr(&stack);
-	// reverse_rotate_a(&stack, 1);
-	// reverse_rotate_b(&stack, 1);
-	push_b(&stack);
+	// swap_a(stack, 1);
+	// swap_b(stack, 1);
+	// ss(stack);
+	// push_a(stack);
+	// push_b(stack);
+	// rotate_a(stack, 1);
+	// rotate_b(stack, 1);
+	// rr(stack);
+	// reverse_rotate_a(stack, 1);
+	// reverse_rotate_b(stack, 1);
+	// push_b(stack);
+
+	// push_a(stack);
 
 
+	if (element == 2)
+		sort_2(stack);
+	if (element ==  3)
+		sort_3(stack);
 
-	// push_a(&stack);
+
 
 	printf("Stack A\n");
-	for (int i = 0; i < stack.a.size; i++)
-		ft_printf("%d %d\n", stack.a.data[i], stack.a.size);
+	for (int i = 0; i < stack->a.size; i++)
+		ft_printf("%d %d\n", stack->a.data[i], stack->a.size);
+	if (stack->a.size == 0) printf("なし\n");
 	printf("Stack B\n");
-	for (int i = 0; i < stack.b.size; i++)
-		ft_printf("%d %d\n", stack.b.data[i], stack.b.size);
-	// printf("%d", stack.a.data[4]);
-
-
+	for (int i = 0; i < stack->b.size; i++)
+		ft_printf("%d %d\n", stack->b.data[i], stack->b.size);
+	if (stack->b.size == 0) printf("なし\n");
 
 }
 
@@ -50,19 +91,22 @@ int	main(int argc, char *argv[])
 {
 	t_stacks stack;
 	int i;
-	int temp;
+	int error;
 
+	error = 0;
 	i = 0;
 	while (i < argc - 1)
 	{
-		temp = ft_atoi(argv[i + 1]);
-		stack.a.data[i] = temp;
+		stack.a.data[i] = ft_safe_atoi(argv[i + 1], &error);
+		if (error)
+		{
+			write(2, "Error\n", 6);
+			return (1);
+		}
 		i++;
 	}
 	stack.a.size = argc - 1;
 	stack.b.size = 0;
-
-	push_swap(stack, argc - 1);
-
+	push_swap(&stack, argc - 1);
 	return (0);
 }
