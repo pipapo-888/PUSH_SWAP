7b8e872cd49efe57c3d799d08ab13b7d1a7880ee
pipapo-888 <kazuya2002uk@gmail.com>
2025-08-31 03:26:21 +0900

    ソート２からソート５完成

---
+++ sort_2_to_5.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_2_to_5.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 00:42:04 by knomura           #+#    #+#             */
+/*   Updated: 2025/08/31 03:18:25 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(t_stack a)
+{
+	int	i;
+
+	i = 0;
+	while (i < a.size - 1)
+	{
+		if (a.data[i].data > a.data[i + 1].data)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	sort_2(t_stacks *stack)
+{
+	if (is_sorted(stack -> a))
+	{
+		printf("ソート済み\n");
+		return ;
+	}
+	else
+		swap_a(stack, 1);
+	printf("ソートしたよ\n");
+}
+
+void	sort_3(t_stacks *stack)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = stack->a.data[0].data;
+	b = stack->a.data[1].data;
+	c = stack->a.data[2].data;
+	if (is_sorted(stack -> a))
+		return ;
+	if (a > b && b > c)
+	{
+		swap_a(stack, 1);
+		reverse_rotate_a(stack, 1);
+	}
+	if (a > b && b < c && a > c)
+		rotate_a(stack, 1);
+	if (a < b && b > c && a < c)
+	{
+		swap_a(stack, 1);
+		rotate_a(stack, 1);
+	}
+	if (a > b && b < c && a < c)
+		swap_a(stack, 1);
+	if (a < b && b > c && a > c)
+		reverse_rotate_a(stack, 1);
+}
+
+void	which_to_push_b(t_stacks *stack, int target)
+{
+	int i;
+
+	i = 0;
+	while (i < stack->a.size && stack->a.data[i].rank != target)
+		i++;
+	while (stack->a.data[0].rank != target)
+	{
+		if (i <= stack->a.size / 2)
+			rotate_a(stack, 1);
+		else
+			reverse_rotate_a(stack, 1);
+	}
+	push_b(stack);
+}
+
+void	sort_5(t_stacks *stack)
+{
+	if (is_sorted(stack->a))
+		return ;
+	which_to_push_b(stack, 0);
+	if (is_sorted(stack->a))
+	{
+		push_a(stack);
+		return ;
+	}
+	which_to_push_b(stack, 1);
+	sort_3(stack);
+	push_a(stack);
+	push_a(stack);
+}
